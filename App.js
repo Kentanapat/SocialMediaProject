dcfaf23181e674e5432f0d7bc685c792653ee101
pmpput <118797624+pmpput@users.noreply.github.com>
2023-09-12 15:33:18 +0700

    update web

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Navbar from './Navbar';
 import Input from './Input';
 import Post from './Post';
+import Sidemenu from './Sidemenu';
 
 
 let id =1;
 
 function App(){
   const[posts,setPosts] = useState([]);
+  const[likes,setLikes] = useState([]);
 
   function addPost(title){
     const newPost = {id,title};
@@ -21,13 +23,19 @@ function App(){
     const updatedPosts = posts.filter((post) => post.id != id);
     setPosts(updatedPosts);
   }
+   function addLikes(likes){
+    const handleLike = () =>{
+      setLikes(likes+1);
+    }
+   }
 
   return (
   <div className="App">
     <Navbar/>
     <Input addPost={addPost} />
     {posts.map((post)=> <Post key={post.id} id={post.id} title={post.title}
-    deletePost={deletePost} />)}
+    deletePost={deletePost}  addLikes={addLikes}/>)}
+    <Sidemenu/>
   </div>);
 }
 export default App;
--- Post.js
+++ Post.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import  PropTypes from 'prop-types';
 
-function Post({id, title , deletePost}){
+function Post({id, title , deletePost, likes ,comment}){
 return(
     <div className="Post">
         <button className="Post-d" onClick={() => deletePost(id)}>X</button>
         <div className="Post-title">{title}</div>
+        <p className="like">Likes: {likes}</p>
+        <p className="com">Comment:</p>
+        <button className="likeclick" onClick={likes}>Like</button>
+        <button className="comclick" onClick={comment}>Comment</button>
+        
         {/* <img className="Post-img" src={`https://source.unplash.com/random?sig=${id}`}
         /> */}
     </div>
@@ -14,6 +19,8 @@ return(
 Post.propTypes = {
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    deletePost:PropTypes.func.isRequired
+    deletePost:PropTypes.func.isRequired,
+    likes: PropTypes.func.isRequired,
+    comment: PropTypes.func.isRequired
 };
 export default Post;
