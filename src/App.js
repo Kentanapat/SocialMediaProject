67ada7b84804c0932d343837a85622a04b4e4f1f
Parinya Keaokraisorn <113964055+nemosandesuka@users.noreply.github.com>
2023-10-14 19:15:48 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NavBar from "./components/NavBar";
+import SignUpForm from "./components/SignUpForm";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [isShowSignUp, setIsShowSignUp] = useState(true);
+
+  const handleSignUpClick = () => {
+    setIsShowSignUp((isShowSignUp) => !isShowSignUp);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NavBar">
+      <NavBar handleSignUpClick={handleSignUpClick}/>
+      <SignUpForm isShowSignUp={isShowSignUp} />
+      </div>
     </div>
   );
 }
-
-export default App;
