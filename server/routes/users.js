ad15058cac59e581db9f69050aefafaa4f0246cb
KenTanapat <118798156+Kentanapat@users.noreply.github.com>
2023-10-22 10:27:17 +0700

    small changes oops

--- server/routes/users.js
+++ server/routes/users.js
@@ -22,7 +22,7 @@ router.put("/:id", async (req, res) => {
       return res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("You can update only your account!");
+    return res.status(403).json("That is not your account.");
   }
 });
 
@@ -36,7 +36,7 @@ router.delete("/:id", async (req, res) => {
       return res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("You can delete only your account!");
+    return res.status(403).json("You can only delete your own account...");
   }
 });
 
@@ -61,15 +61,15 @@ router.put("/:id/follow", async (req, res) => {
       if (!user.followers.includes(req.body.userId)) {
         await user.updateOne({ $push: { followers: req.body.userId } });
         await currentUser.updateOne({ $push: { followings: req.params.id } });
-        res.status(200).json("user has been followed");
+        res.status(200).json("User has been followed");
       } else {
-        res.status(403).json("you allready follow this user");
+        res.status(403).json("User was already followed");
       }
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("you cant follow yourself");
+    res.status(403).json("You can't follow yourself...");
   }
 });
 
@@ -83,15 +83,15 @@ router.put("/:id/unfollow", async (req, res) => {
         if (user.followers.includes(req.body.userId)) {
           await user.updateOne({ $pull: { followers: req.body.userId } });
           await currentUser.updateOne({ $pull: { followings: req.params.id } });
-          res.status(200).json("user has been unfollowed");
+          res.status(200).json("Successfully unfollowed the user");
         } else {
-          res.status(403).json("you dont follow this user");
+          res.status(403).json("User was never followed");
         }
       } catch (err) {
         res.status(500).json(err);
       }
     } else {
-      res.status(403).json("you cant unfollow yourself");
+      res.status(403).json("you cant unfollow yourself...");
     }
   });
 
